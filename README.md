0681ee29037300e3035b180972ece62c35618cfd
Aaron Rasmussen <aaron.rasmussen@byu.edu>
2017-07-19 14:00:08 -0600

    initial

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # handel-utils
-Utility library for apps deployed using handel and handel-codepipeline
+Utility library for apps deployed using [handel](https://github.com/byu-oit/handel) and
+[handel-codepipeline](https://github.com/byu-oit/handel-codepipeline)
+
+# Environment Variables
+
+## getVariable
+Retrieves the value of an environment variable injected by Handel
+
+Parameters:
+- `serviceType`: the type of the service exporting the variable (for example `'s3'`)
+- `serviceName`: the name of the service as configured in your `handel.yml` file
+- `variableName`: the variable to retrieve (for example `'BUCKET_NAME'`)
+
+## handelEnv
+Retrieves environment variables injected into each service by Handel
+
+Example result:
+```
+  {
+    appName: 'TEST_APP',
+    envName: 'DEV',
+    serviceName: 'TESTARTIFACT',
+    serviceVersion: 'V1'
+  }
+```
+
+
+## partialPrefix
+Builds the part of the Environment Variable Prefix which is derived from the application name and
+environment name.
+([see Handel documentation](http://handel.readthedocs.io/en/latest/handel-basics/consuming-service-dependencies.html#environment-variable-prefix))
+
+## servicePartialPrefix
+Validates that the provided service type is recognized by Handel and injects variables, then returns
+a partial prefix.
+
+Parameters:
+- `serviceType`
+
+The valid service types are:
+- dynamodb
+- efs
+- memcached
+- mysql
+- postgresql
+- redis
+- s3
+- sns
+- sqs
---
+++ test/testEnvironment.js
@@ -0,0 +1,88 @@
+const test = require('tape')
+const util = require('../index')
+// const _ = require('lodash')
+
+const testVars = {
+  HANDEL_APP_NAME: 'TEST_APP',
+  HANDEL_ENVIRONMENT_NAME: 'DEV',
+  HANDEL_SERVICE_NAME: 'TESTARTIFACT',
+  HANDEL_SERVICE_VERSION: 'V1',
+  DYNAMODB_TEST_APP_DEV_MYTABLE_TABLE_NAME: 'mytable',
+  DYNAMODB_TEST_APP_DEV_MYTABLE_TABLE_ARN: 'arn:faketestvaluefortable',
+  EFS_TEST_APP_DEV_MYEFS_MOUNT_DIR: '/fake/mountdir',
+  MEMCACHED_TEST_APP_DEV_MYELASTICACHE_ADDRESS: 'my_cache.fake.com',
+  MEMCACHED_TEST_APP_DEV_MYELASTICACHE_PORT: '9999',
+  MYSQL_TEST_APP_DEV_MYDB_ADDRESS: 'my_db.fake.com',
+  MYSQL_TEST_APP_DEV_MYDB_PORT: '8888',
+  MYSQL_TEST_APP_DEV_MYDB_USERNAME: 'fakeuser',
+  MYSQL_TEST_APP_DEV_MYDB_DATABASE_NAME: 'fakedb',
+  POSTGRESQL_TEST_APP_DEV_MYDB_ADDRESS: 'my_db2.fake.com',
+  POSTGRESQL_TEST_APP_DEV_MYDB_PORT: '8989',
+  POSTGRESQL_TEST_APP_DEV_MYDB_USERNAME: 'fakeuser2',
+  POSTGRESQL_TEST_APP_DEV_MYDB_DATABASE_NAME: 'fakedb2',
+  REDIS_TEST_APP_DEV_MYCACHE_ADDRESS: 'my_cache2.fake.com',
+  REDIS_TEST_APP_DEV_MYCACHE_PORT: '9898',
+  S3_TEST_APP_DEV_MYBUCKET_BUCKET_NAME: 'my_bucket',
+  S3_TEST_APP_DEV_MYBUCKET_BUCKET_URL: 'my_bucket.fake.com',
+  S3_TEST_APP_DEV_MYBUCKET_REGION_ENDPOINT: 'us-west-2',
+  SNS_TEST_APP_DEV_MYTOPIC_TOPIC_ARN: 'arn:faketestvaluefortopic',
+  SNS_TEST_APP_DEV_MYTOPIC_TOPIC_NAME: 'testtopic',
+  SQS_TEST_APP_DEV_MYQUEUE_QUEUE_NAME: 'myqueue',
+  SQS_TEST_APP_DEV_MYQUEUE_QUEUE_URL: 'my_queue.fake.com',
+  SQS_TEST_APP_DEV_MYQUEUE_QUEUE_ARN: 'arn:faketestvalueforqueue'
+}
+process.env = Object.assign({}, process.env, testVars)
+
+const serviceList = [
+  'dynamodb',
+  'efs',
+  'memcached',
+  'mysql',
+  'postgresql',
+  'redis',
+  's3',
+  'sns',
+  'sqs'
+]
+
+test('Handel Environment', assert => {
+  const actual = util.handelEnv()
+  const expected = {
+    appName: 'TEST_APP',
+    envName: 'DEV',
+    serviceName: 'TESTARTIFACT',
+    serviceVersion: 'V1'
+  }
+  assert.deepEqual(actual, expected, 'Correctly loaded handel common variables')
+  assert.end()
+})
+
+test('Partial Environment Prefix', assert => {
+  const actual = util.partialPrefix()
+  const expected = 'TEST_APP_DEV'
+  assert.equal(actual, expected, 'Correctly generated a partial environmental prefix')
+  assert.end()
+})
+
+test('Each service prefix', assert => {
+  assert.plan(serviceList.length)
+  serviceList.forEach(service => {
+    const actual = util.servicePartialPrefix(service)
+    const expected = `${service.toUpperCase()}_TEST_APP_DEV`
+    assert.equal(actual, expected, `${service} service prefix generated correctly`)
+  })
+})
+
+test('Service variable', assert => {
+  const actual = util.getVariable('s3', 'MYBUCKET', 'BUCKET_NAME')
+  const expected = 'my_bucket'
+  assert.equal(actual, expected, 'Correctly retrieved a variable')
+
+  assert.throws(() => util.getVariable('invalid', 'ITEM', 'VAR_NAME'), /is not a recognized handel service/, 'Expected invalid service name to throw an error')
+
+  assert.throws(() => util.getVariable('s3', 3, 'VAR_NAME'), /Expected the configuration/, 'Expected invalid configuration item name to throw an error')
+
+  assert.throws(() => util.getVariable('s3', 'MYBUCKET', 'VAR_NAME'), /not a recognized handel variable/, 'Expected invalid variable name to throw an error')
+
+  assert.end()
+})
